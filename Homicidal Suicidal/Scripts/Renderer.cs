6129ed06e16300c485c8756ce42767cab5c9b6a0
Sixten Schelin <crazysissen@gmail.com>
2018-04-17 13:45:43 +0200

    Hooold up

--- Homicidal Suicidal/Scripts/Renderer.cs
+++ Homicidal Suicidal/Scripts/Renderer.cs
@@ -11,6 +11,11 @@ namespace HomicidalSuicidal
 {
     static class Renderer
     {
+        public static Vector2 camera;
+        public static Vector2 offset = new Vector2(20, 0);
+
+        static Vector2 CameraScreenPosition => camera + offset - new Vector2((float)Game1.graphics.PreferredBackBufferWidth * 0.5f, (float)Game1.graphics.PreferredBackBufferWidth * 0.5f);
+
         public static List<IRenderable> Renderables
         {
             get
@@ -29,22 +34,22 @@ namespace HomicidalSuicidal
             }
         }
 
-        public static void RenderAll(SpriteBatch spriteBatch, Point camera)
+        public static void RenderAll(SpriteBatch spriteBatch)
         {
             List<IRenderable> renderables = Renderables;
             foreach (IRenderable renderable in renderables)
             {
-                Render(renderable, Game1.MainSpriteBatch, camera);
+                Render(renderable, Game1.MainSpriteBatch, CameraScreenPosition);
             }
         }
 
-        public static void RenderAll(SpriteBatch spriteBatch)
-            => RenderAll(spriteBatch, Point.Zero);
+        //public static void RenderAll(SpriteBatch spriteBatch)
+        //    => RenderAll(spriteBatch, Point.Zero);
 
-        public static void Render(IRenderable renderable, SpriteBatch spriteBatch, Point camera)
-            => spriteBatch.Draw(renderable.Sprite, renderable.Rect.Location.ToVector2(), null, renderable.SpriteColor, renderable.Rotation, Vector2.Zero, renderable.Rect.Size.ToVector2() / new Vector2(renderable.Sprite.Width, renderable.Sprite.Height), SpriteEffects.None, 0);
+        public static void Render(IRenderable renderable, SpriteBatch spriteBatch, Vector2 camera)
+            => spriteBatch.Draw(renderable.Sprite, renderable.Rect.Location.ToVector2() - camera, null, renderable.SpriteColor, renderable.Rotation, Vector2.Zero, renderable.Rect.Size.ToVector2() / new Vector2(renderable.Sprite.Width, renderable.Sprite.Height), SpriteEffects.None, 0);
 
-        public static void RenderThis(this IRenderable renderable, SpriteBatch spriteBatch, Point camera)
+        public static void RenderThis(this IRenderable renderable, SpriteBatch spriteBatch, Vector2 camera)
             => Render(renderable, spriteBatch, camera);
     }
 }
