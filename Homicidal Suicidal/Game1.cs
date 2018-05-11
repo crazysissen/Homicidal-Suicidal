9e78d44e2d44a5fa1acd03eec00d503202906e78
Benjamin Ek <benjamin.ek1@learnet.se>
2018-05-11 21:54:54 +0200

    asd

--- Homicidal Suicidal/Game1.cs
+++ Homicidal Suicidal/Game1.cs
@@ -139,7 +139,7 @@ namespace HomicidalSuicidal
 
         void IntroductionUpdate()
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
                 CurrentState.Push(GameState.InGame);
 
             Point screenSize = new Point(Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight);
@@ -149,7 +149,7 @@ namespace HomicidalSuicidal
                 new GUI.Label("I want to die.", 100, new Vector2(100, 100), menuFont, Color.White),
                 new GUI.Label("They won't let me die.", 100, new Vector2(100, 300), menuFont, Color.White),
                 new GUI.Label("They must die.", 100, new Vector2(100, 500), menuFont, Color.White),
-                new GUI.Label("Press Enter to continue", 60, new Vector2(900, 1000), defaultFont, Color.White)
+                new GUI.Label("Press Space to continue", 60, new Vector2(900, 990), defaultFont, Color.White)
                 );
         }
 
